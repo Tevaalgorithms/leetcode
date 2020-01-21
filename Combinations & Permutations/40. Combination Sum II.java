658acd51b5f094dfcd836b1b5448b4657a10a854
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2020-01-20 21:56:36 -0700

    Update 40. Combination Sum II.java

--- Combinations & Permutations/40. Combination Sum II.java
+++ Combinations & Permutations/40. Combination Sum II.java
@@ -1,28 +1,45 @@
-// helpful URL: https://www.youtube.com/watch?v=IER1ducXujU
+// Help: https://www.youtube.com/watch?v=j9_qWJClp64
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();       
+        
+        if(candidates == null || candidates.length == 0) {
+            return result;
+        }
+        
+         // Here we are soring to get smaller elements to come first in DFS
         Arrays.sort(candidates);
-        combinationSum(candidates, 0, target, new ArrayList<Integer>(), results);
-        return results;        
+        
+        List<Integer> combinations = new ArrayList<>();
+        
+        toFindCombinations(candidates, target, result, combinations, 0);
+        
+        return result;
     }
-         
-    private void combinationSum(int[] candidates, int index, int target, ArrayList<Integer> current,  List<List<Integer>> results) {
-        if(target == 0) {
-            results.add(new ArrayList<>(current));
+    
+    private void toFindCombinations(int[] candidates, int target, List<List<Integer>> result, List<Integer> combinations, int startIndex) {
+        
+        if(target == 0){
+            // Note: Here we need to do a deep copy
+            result.add(new ArrayList<>(combinations));
             return;
         }
         
-        if(target < 0) {
-            return;
-        }
-        
-        for(int i = index; i < candidates.length; i++) {
-            if(i == index || candidates[i] != candidates[i - 1]){
-                current.add(candidates[i]);
-                combinationSum(candidates, i + 1, target - candidates[i], current, results);
-                current.remove(current.size() - 1);
+        for(int i = startIndex; i < candidates.length; i++) {
+            // We don't need to do the same elements twice 
+            if(i != startIndex && candidates[i] == candidates[i - 1]){
+                continue;
             }
+            
+            if(candidates[i] > target){
+                break;
+            }
+            
+            combinations.add(candidates[i]);
+            
+            toFindCombinations(candidates, target - candidates[i], result, combinations, i + 1);
+            
+            combinations.remove(combinations.size() - 1);
         }
     }
     
