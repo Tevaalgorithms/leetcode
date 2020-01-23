abe371e03b734fc0bfbba6140b0ac908aae73a01
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2020-01-22 23:29:56 -0700

    Create 144. Binary Tree Preorder Traversal.java

---
+++ Trees/Tree Traversal/144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        
+        if(root == null) {
+            return result;
+        }
+        
+        helper(root, result);
+        
+        return result;
+    }
+    
+    // PreOrder: Root > Left > Right
+    private void helper(TreeNode root, List<Integer> result) {
+        if(root == null) {
+            return;
+        }
+        result.add(root.val);
+        helper(root.left, result);
+        helper(root.right, result);
+    }
+}
