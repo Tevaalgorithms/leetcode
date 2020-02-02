24c9cdfe0162bb858ae6efa0b127ff2591077559
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2020-02-02 02:07:20 -0700

    Create 1. Two Sum.java

---
+++ HashMap/1. Two Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if(hm.containsKey(temp)){
+                return new int[] { hm.get(temp), i };
+            } else {
+                hm.put(nums[i], i);
+            }
+        }
+       throw new IllegalArgumentException("No two sum solution");
+    }
+}
