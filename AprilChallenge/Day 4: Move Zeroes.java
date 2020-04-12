c4b63a86fccecbb4916fc09cda03a6c200ae81bc
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2020-04-12 16:49:47 -0600

    Create Day 4: Move Zeroes.java

---
+++ AprilChallenge/Day 4: Move Zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int i = 0;
+       int j = 0;
+        
+       while(j < nums.length) {
+           if(nums[j] != 0) {
+               nums[i++] = nums[j];
+           }
+           j++;
+       }
+       
+       while(i < nums.length) {
+           nums[i++] = 0;
+       }
+        
+    }
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
