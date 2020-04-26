43b796f1b19420d7b43d0493c541abc86d00b202
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2020-04-26 12:17:26 -0600

    Create Day 8: Middle of the Linked List.java

---
+++ AprilChallenge/Day 8: Middle of the Linked List.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slowPtr = head, fastPtr = head;
+        while(fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+        return slowPtr;
+    }
+}
+
+// Time Complexity: O(N)
+
+// Space Complecity: O(1)
